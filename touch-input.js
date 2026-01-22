81a9666b9370c860245cddf6c539fcbd4260a255
Jukka Aho <143696182+EyzeCOLD@users.noreply.github.com>
2026-01-22 20:32:09 +0200

    Refactor touch input handling to use static properties

--- touch-input.js
+++ touch-input.js
@@ -12,21 +12,20 @@ const TouchInput = {
    */
   handleTouchStart(evt) {
     const touch = evt.touches[0];
-    this.x0 = touch.clientX;
-    this.y0 = touch.clientY;
+    TouchInput.x0 = touch.clientX;
+    TouchInput.y0 = touch.clientY;
   },
 
   /**
    * Figure out the direction of the gesture from the delta
    */
   handleTouchMove(evt) {
-    if (!this.x0 || !this.y0) return;
+    if (!TouchInput.x0 || !TouchInput.y0) return;
 
     const x1 = evt.touches[0].clientX;
     const y1 = evt.touches[0].clientY;
-    const xD = this.x0 - x1;
-    const yD = this.y0 - y1;
-	  console.log(`xD: ${xD} yD: ${yD}`);
+    const xD = TouchInput.x0 - x1;
+    const yD = TouchInput.y0 - y1;
 
     if (Math.abs(xD) > Math.abs(yD)) {
       if (xD > 0) Game.move("ArrowLeft");
@@ -35,8 +34,8 @@ const TouchInput = {
       if (yD > 0) Game.move("ArrowUp");
       else Game.move("ArrowDown");
     }
-    this.x0 = null;
-    this.y0 = null;
+    TouchInput.x0 = null;
+    TouchInput.y0 = null;
 
     evt.preventDefault();
   },
