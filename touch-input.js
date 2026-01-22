54fda7cad930011906c06354719d99dcde8ba691
juaho <juaho@student.hive.fi>
2026-01-22 21:19:52 +0200

    Put the touch controls inside my first closure, fixing the this bugs from the object implementation

--- touch-input.js
+++ touch-input.js
@@ -1,31 +1,25 @@
-document.addEventListener("touchstart", TouchInput.handleTouchStart, false);
-document.addEventListener("touchmove", TouchInput.handleTouchMove, {
-  passive: false,
-});
-
-const TouchInput = {
-  x0: null,
-  y0: null,
-
+function initTouchInputHandling() {
+  x0 = null;
+  xy = null;
   /**
    * Detect the starting coordinate of the touch gesture
    */
-  handleTouchStart(evt) {
+  function handleTouchStart(evt) {
     const touch = evt.touches[0];
-    TouchInput.x0 = touch.clientX;
-    TouchInput.y0 = touch.clientY;
-  },
+    x0 = touch.clientX;
+    y0 = touch.clientY;
+  }
 
   /**
    * Figure out the direction of the gesture from the delta
    */
-  handleTouchMove(evt) {
-    if (!TouchInput.x0 || !TouchInput.y0) return;
+  function handleTouchMove(evt) {
+    if (x0 == null || y0 == null) return;
 
     const x1 = evt.touches[0].clientX;
     const y1 = evt.touches[0].clientY;
-    const xD = TouchInput.x0 - x1;
-    const yD = TouchInput.y0 - y1;
+    const xD = x0 - x1;
+    const yD = y0 - y1;
 
     if (Math.abs(xD) > Math.abs(yD)) {
       if (xD > 0) Game.move("ArrowLeft");
@@ -34,9 +28,16 @@ const TouchInput = {
       if (yD > 0) Game.move("ArrowUp");
       else Game.move("ArrowDown");
     }
-    TouchInput.x0 = null;
-    TouchInput.y0 = null;
+    x0 = null;
+    y0 = null;
 
     evt.preventDefault();
-  },
-};
+  }
+
+  document.addEventListener("touchstart", handleTouchStart, false);
+  document.addEventListener("touchmove", handleTouchMove, {
+    passive: false,
+  });
+}
+
+initTouchInputHandling();
